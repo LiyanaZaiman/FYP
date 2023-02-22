63557e5731d52673cee85fd8c24205b21443d070
LiyanaZaiman <nurliyana0230@gmail.com>
2023-02-22 22:25:28 +0800

    RResize

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -262,23 +262,117 @@ Widget build(BuildContext context) {
             children: [
                 Container(
                 width: 1200,
-                height: 600,
+                height: 700,
                 color: Colors.white70,
               ),
-              SizedBox( // <-- SEE HERE
-                width: 200,
-                child: TextField(
-                decoration: InputDecoration(
-                hintText: 'name',
-                labelText: 'Enter Name',
-                border: OutlineInputBorder(),
+              //Name
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 100,
+                  left: 200,
+                ),
+                child: SizedBox( // <-- SEE HERE
+                  width: 200,
+                  child: TextField(
+                  decoration: InputDecoration(
+                  hintText: 'Enter name',
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                  ),
+                ),
+                          ),
+              ),
+              //Contact Number
+                Padding(
+                padding: const EdgeInsets.only(
+                  top: 180,
+                  left: 200,
+                ),
+                child: SizedBox(
+                  width: 200,
+                  child: TextField(
+                  decoration: InputDecoration(
+                  hintText: 'Enter contact number',
+                  labelText: 'Contact Number',
+                  border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                          ),
+              ),
+              //Label
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 250,
+                  left: 200,
+                  ),
+                child: Text(
+                'Delivery',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
+              //Address Line 1
+                Padding(
+                padding: const EdgeInsets.only(
+                  top: 300,
+                  left: 200,
+                ),
+                child: SizedBox(
+                  width: 200,
+                  child: TextField(
+                  decoration: InputDecoration(
+                  hintText: 'Enter your adress',
+                  labelText: 'Address Line',
+                  border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                          ),
+              ),
+              //Address Line 2
+                Padding(
+                padding: const EdgeInsets.only(
+                  top: 380,
+                  left: 200,
+                ),
+                child: SizedBox(
+                  width: 200,
+                  child: TextField(
+                  decoration: InputDecoration(
+                  hintText: 'Enter your adress',
+                  labelText: 'Address Line',
+                  border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                          ),
+              ),
+              //Postcode
+                Padding(
+                padding: const EdgeInsets.only(
+                  top: 460,
+                  left: 200,
+                ),
+                child: SizedBox(
+                  width: 200,
+                  child: TextField(
+                  decoration: InputDecoration(
+                  hintText: 'Enter your postcode',
+                  labelText: 'Postcode',
+                  border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                          ),
+              ),
+              //Button
               Center(
                 child: Padding(
                  padding: EdgeInsets.only(
-                  top: 500,
+                  top: 550,
                  ),
                   child: ElevatedButton(
                     child: Text('Order Now'),
